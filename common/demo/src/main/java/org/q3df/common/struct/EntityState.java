4be1f93cd40a8d8d016426f652fb6c91062ecdb8
michael-rd <michael.zavedeev@gmail.com>
2017-06-14 20:02:49 +0300

    stage 2, add read/write methods

---
+++ common/demo/src/main/java/org/q3df/common/struct/EntityState.java
@@ -0,0 +1,7 @@
+package org.q3df.common.struct;
+
+/**
+ * Created by michael on 14.06.17.
+ */
+public class EntityState {
+}
---
+++ common/demo/src/main/java/org/q3df/common/struct/UserCmd.java
@@ -0,0 +1,12 @@
+package org.q3df.common.struct;
+
+/**
+ * Created by michael on 14.06.17.
+ */
+public class UserCmd {
+    int serverTime;
+    int angles[];
+    int buttons;
+    byte weapon;
+    byte forwardMove, rightMove, upMove;
+}
